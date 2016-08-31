54d5403418a010604cddd98d88a5e8693fb43492
Roger Chien <pshadow.tw@gmail.com>
2016-08-31 00:04:08 -0700

    Initial commit. video part1 finished

---
+++ Form.elm
@@ -0,0 +1,64 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App
+import Zipcode
+
+
+type alias Model =
+    { zipcode : Maybe String
+    , zipcodeEntry : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { zipcode = Nothing
+    , zipcodeEntry = ""
+    }
+
+
+
+-- subscriptions :
+
+
+type Msg
+    = ZipcodeChanged String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case Debug.log "update" msg of
+        ZipcodeChanged newEntry ->
+            { model | zipcodeEntry = newEntry }
+
+
+view : Model -> Html Msg
+view model =
+    Html.form
+        [ class "pure-form pure-form-aligned" ]
+        [ fieldset []
+            [ div [ class "pure-control-group" ]
+                [ label [ for "zipcode" ] [ text "Zipcode" ]
+                , input
+                    [ id "zipcode"
+                    , type' "text"
+                    , classList
+                        [ ( "input-invalid", not (Zipcode.isValid model.zipcodeEntry) ) ]
+                    , onInput ZipcodeChanged
+                    ]
+                    []
+                ]
+            ]
+        ]
+
+
+main : Program Never
+main =
+    Html.App.beginnerProgram
+        { model = initialModel
+        , update = update
+        , view = view
+        }
